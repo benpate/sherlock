c650f73edb7e6f5105c5966d2e8ed656c78867b7
Ben Pate <ben@pate.org>
2023-08-03 15:21:22 -0600

    Fix for CSS-Tricks
    It looks like WordPress uses "application/json" link types to include WordPress-specific data.  Until we want to parse WORDPRESS-JSON, this is right out.

--- utils.go
+++ utils.go
@@ -9,7 +9,7 @@ func isActivityStream(value string) bool {
 	// ActivityStreams have their own MIME type, but we have to check some alternates, too.
 	if mediaType, _, err := mime.ParseMediaType(value); err == nil {
 		switch mediaType {
-		case "application/activity+json", "application/ld+json", "application/json":
+		case "application/activity+json", "application/ld+json":
 			return true
 		}
 	}
