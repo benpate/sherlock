b46d203c3136d54747252a7cf7b76c54b707d3e5
Ben Pate <ben@pate.org>
2023-08-03 23:07:00 -0600

    Placeholder for WP-JSON

---
+++ parse-wordpress.go
@@ -0,0 +1,4 @@
+package sherlock
+
+// TODO: Add support for WordPress-specific JSON links
+// https://enappd-apps.gitbook.io/apps/ionic-4-full-app/startup-features/wordpress/wordpress-json-api-basics
