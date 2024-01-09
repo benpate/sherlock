c9259071e6bd66e6b46317a649848f38e0af0c51
Ben Pate <ben@pate.org>
2024-01-09 15:17:08 -0700

    Add IsValidAddress verification function

---
+++ sherlock-extras.go
@@ -0,0 +1,38 @@
+package sherlock
+
+import (
+	"net/url"
+	"strings"
+)
+
+// IsValidAddress returns TRUE for all values that Sherlock THINKS it SHOULD
+// be able to prorcess.  This includes: @username@host.tld and https://host.tld/username
+// addresses.
+// IMPORTANT: Just because this function returns TRUE does NOT mean that the address
+// is valid.  It just means that it looks like a valid format, but it will still need
+// to be checked.
+func IsValidAddress(address string) bool {
+
+	// If this LOOKS LIKE a username, then try to split into username and domain
+	if strings.HasPrefix(address, "@") {
+
+		address = strings.TrimPrefix(address, "@")
+
+		if _, domain, found := strings.Cut(address, "@"); found {
+			address = "https://" + domain
+		}
+
+		// Fall through to check that the domain is valid
+	}
+
+	// Validate that the address is a valid URL
+	if strings.HasPrefix(address, "https://") || strings.HasPrefix(address, "http://") {
+
+		if _, err := url.Parse(address); err == nil {
+			return true
+		}
+	}
+
+	// If we get here, then the address is not valid
+	return false
+}
