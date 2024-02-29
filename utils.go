378f5e67a876e7e9392a9791fbe313ea77394d32
Ben Pate <ben@pate.org>
2024-02-29 11:45:36 -0700

    Add logging utilities

--- utils.go
+++ utils.go
@@ -55,7 +55,32 @@ func withContext(value mapof.Any) {
 	}
 }
 
-// canLog returns TRUE if the current logging level is supported
+// canLog is a silly zerolog helper that returns TRUE
+// if the provided log level would be allowed
+// (based on the global log level).
+// This makes it easier to execute expensive code conditionally,
+// for instance: marshalling a JSON object for logging.
 func canLog(level zerolog.Level) bool {
-	return level >= zerolog.GlobalLevel()
+	return zerolog.GlobalLevel() <= level
+}
+
+// canTrace returns TRUE if zerolog is configured to allow Trace logs
+// This function is here for completeness.  It may or may not be used
+// nolint: unused
+func canTrace() bool {
+	return canLog(zerolog.TraceLevel)
+}
+
+// canDebug returns TRUE if zerolog is configured to allow Debug logs
+// This function is here for completeness.  It may or may not be used
+// nolint: unused
+func canDebug() bool {
+	return canLog(zerolog.DebugLevel)
+}
+
+// canInfo returns TRUE if zerolog is configured to allow Info logs
+// This function is here for completeness.  It may or may not be used
+// nolint: unused
+func canInfo() bool {
+	return canLog(zerolog.InfoLevel)
 }
