80dcff069d6b8d6546afbaf922df4bea3b22c30a
Ben Pate <ben@pate.org>
2025-08-21 10:21:48 -0600

    Better error reporting for failed ActivityStreams

--- document-activityStream.go
+++ document-activityStream.go
@@ -1,18 +1,24 @@
 package sherlock
 
 import (
+	"github.com/benpate/derp"
 	"github.com/benpate/hannibal/streams"
 	"github.com/benpate/hannibal/vocab"
 	"github.com/benpate/remote"
+	"github.com/benpate/remote/options"
 	"github.com/benpate/rosetta/mapof"
 )
 
 // loadDocument_ActivityStream tries to load a remote document as an ActivityStream
 // If successful, it will return a streams.Document with the appropriate metadata.
 // Otherwise, it returns a nil document.
-func (client *Client) loadDocument_ActivityStream(config Config, uri string) streams.Document {
+func (client *Client) loadDocument_ActivityStream(config Config, uri string) (streams.Document, error) {
+
+	const location = "sherlock.client.loadDocument_ActivityStream"
 
 	data := mapof.NewAny()
+
+	config.RemoteOptions = append(config.RemoteOptions, options.Debug())
 
 	txn := remote.Get(uri).
 		UserAgent(config.UserAgent).
@@ -21,16 +27,16 @@ func (client *Client) loadDocument_ActivityStream(config Config, uri string) str
 		Result(&data)
 
 	if err := txn.Send(); err != nil {
-		return streams.NilDocument()
+		return streams.NilDocument(), derp.Wrap(err, location, "Unable to load remote document", uri)
 	}
 
 	if !isActivityStream(txn.ResponseContentType()) {
-		return streams.NilDocument()
+		return streams.NilDocument(), nil
 	}
 
 	return streams.NewDocument(
 		data,
 		streams.WithClient(client),
 		streams.WithHTTPHeader(txn.ResponseHeader()),
-	)
+	), nil
 }
